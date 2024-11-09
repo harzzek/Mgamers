627c06888e4ec22f5b32a9b6434255cec95f88fd
Hans Christian Leth-Nissen <39692510+harzzek@users.noreply.github.com>
2024-11-09 22:17:07 +0100

    Updated values for first release

--- frontend/mgamerswebapp/src/stores/eventStore.tsx
+++ frontend/mgamerswebapp/src/stores/eventStore.tsx
@@ -1,6 +1,8 @@
 import axiosInstance from "./axiosInstance";
 
-const API_URL = "http://localhost:8080/api/Event";
+const API_URL = "http://87.59.249.95:8080/api/Event";
+
+const API_URL_REGISTRATION = "http://87.59.249.95:8080/api/Registration";
 
 export const fetchEvents = async () => {
     const response = await axiosInstance.get(`${API_URL}`);
@@ -20,17 +22,17 @@ export const createEvent = async (data: { name: string; description: string; loc
 };
 
 export const registerForEvent = async (eventId: number, userId: number, seatids: number[]) => {
-    const response = await axiosInstance.post(`http://localhost:8080/api/Registration`, { userId, eventId, seatids });
+    const response = await axiosInstance.post(`${API_URL_REGISTRATION}`, { userId, eventId, seatids });
     return response.data;
 };
 
 export const unregisterFromEvent = async (eventId: number, userId: number) => {
-    const response = await axiosInstance.delete(`http://localhost:8080/api/Registration`, { data: { userId, eventId } });
+    const response = await axiosInstance.delete(`${API_URL_REGISTRATION}`, { data: { userId, eventId } });
     return response.data;
 
 };
 
 export const extendSeat = async (userId: number, eventId: number, seatIds: number[]) => {
-    const response = await axiosInstance.post(`http://localhost:8080/api/Registration/admin`, {userId, eventId, seatIds});
+    const response = await axiosInstance.post(`${API_URL_REGISTRATION}/admin`, {userId, eventId, seatIds});
     return response.data;
 }
