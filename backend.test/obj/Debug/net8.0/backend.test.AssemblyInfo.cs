a721fa591f71ec58fe2e6bb717b6fac02ce3c01d
Hans Christian Leth-Nissen <39692510+harzzek@users.noreply.github.com>
2024-11-30 17:25:12 +0100

    registration service tests

--- backend.test/obj/Debug/net8.0/backend.test.AssemblyInfo.cs
+++ backend.test/obj/Debug/net8.0/backend.test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d133c60148834327d148f0d81e03007d1e0fbfb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e0716ac5e2cce39071356ad49f955e2ec2e79a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
