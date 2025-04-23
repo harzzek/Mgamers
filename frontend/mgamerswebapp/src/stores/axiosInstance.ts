5d54c17a019b452a5ce9f25e5d38c034f4994ff7
Hans Christian Leth-Nissen <39692510+harzzek@users.noreply.github.com>
2025-04-23 17:48:31 +0200

    Revert change to frontend

--- frontend/mgamerswebapp/src/stores/axiosInstance.ts
+++ frontend/mgamerswebapp/src/stores/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_DOMAIN_IP}/api`,
+    baseURL: `http://localhost:8080/api`,
     headers: {
         'Content-Type': 'application/json',
     },
