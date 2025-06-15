f2eb603e7ed1b463742cfe6668f58b2b505fc08d
Hans Christian Leth-Nissen <39692510+harzzek@users.noreply.github.com>
2025-06-15 17:46:03 +0200

    Removed unneeded theme

--- frontend/mgamerswebapp/tailwind.config.ts
+++ frontend/mgamerswebapp/tailwind.config.ts
@@ -20,42 +20,6 @@ const config: Config = {
   plugins: [heroui(
     {
       themes: {
-        "purple-dark": {
-          extend: "dark", // <- inherit default values from dark theme
-          colors: {
-            background: "#0D001A",
-            foreground: "#F5FFFA",
-            primary: {
-              50: "#3B096C",
-              100: "#520F83",
-              200: "#7318A2",
-              300: "#9823C2",
-              400: "#c031e2",
-              500: "#DD62ED",
-              600: "#F182F6",
-              700: "#FCADF9",
-              800: "#FDD5F9",
-              900: "#FEECFE",
-              DEFAULT: "#DD62ED",
-              foreground: "#0D001A",
-            },
-            focus: "#F182F6",
-          },
-          layout: {
-            disabledOpacity: "0.3",
-            radius: {
-              small: "4px",
-              medium: "6px",
-              large: "8px",
-            },
-            borderWidth: {
-              small: "1px",
-              medium: "2px",
-              large: "3px",
-            },
-          },
-        },
-
         "mgamers-dark": {
           extend: "dark", // Inherit default dark values
           colors: {
